037ccd854bd50ed01dc5a105b24a8c05c71831fa
Войткович Екатерина <115071542+Voitkovich@users.noreply.github.com>
2023-07-29 18:43:58 +0300

    Update HW_3.py

--- HW_3.py
+++ HW_3.py
@@ -1,15 +1,15 @@
 # Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами. 
 # В результирующем списке не должно быть дубликатов. [1, 2, 3, 1, 2, 4, 5] -> [1, 2]
 
-# def get_unique_num(numbers):
-#     uniq = []
+def get_unique_num(numbers):
+    uniq = []
 
-#     for number in numbers:
-#         if number in uniq:
-#             continue
-#         else:
-#             uniq.append(number)
-#     return uniq
+    for number in numbers:
+        if number in uniq:
+            continue
+        else:
+            uniq.append(number)
+    return uniq
 
 
 # numbers = [1, 2, 5, 3, 1, 77, 32, 77, 44, 1, 6]
